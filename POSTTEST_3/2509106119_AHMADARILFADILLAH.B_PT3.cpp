adbf130ac504639f04c06a369e177d82b755a5cd
ahmadarilfadillah1222 <ahmadarilfadilah1222@gmail.com>
2026-04-11 19:22:21 +0800

    sdh diperbaiki

--- POSTTEST_3/2509106119_AHMADARILFADILLAH.B_PT3.cpp
+++ POSTTEST_3/2509106119_AHMADARILFADILLAH.B_PT3.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 using namespace std;
 
+#define MAX 100
+
 struct Hewan {
     int id;
     string nama;
@@ -23,10 +25,10 @@ struct Riwayat {
 int n = 0;
 
 int front = -1, rear = -1;
-Antrian queueArr[100];
+Antrian queueArr[MAX];
 
 int top = -1;
-Riwayat stackArr[100];
+Riwayat stackArr[MAX];
 
 void swapData(Hewan *a, Hewan *b){
     Hewan temp = *a;
@@ -40,6 +42,11 @@ void tambahData(Hewan *arr){
     cin >> x;
 
     for(int i = 0; i < x; i++){
+        if(n == MAX){
+            cout << "\nData penuh!\n";
+            return;
+        }
+
         cout << "\nData ke-" << i+1 << endl;
         cout << "ID    : "; cin >> (arr + n)->id;
         cout << "Nama  : "; cin >> (arr + n)->nama;
@@ -50,6 +57,11 @@ void tambahData(Hewan *arr){
 }
 
 void tampilData(Hewan *arr){
+    if(n == 0){
+        cout << "\nData kosong!\n";
+        return;
+    }
+
     cout << "\n====================================================\n";
     cout << setw(5) << "ID"
          << setw(15) << "Nama"
@@ -65,106 +77,18 @@ void tampilData(Hewan *arr){
     }
 }
 
-void linearSearch(Hewan *arr){
-    string cari;
-    cout << "\nCari nama: ";
-    cin >> cari;
-
-    for(int i = 0; i < n; i++){
-        if((arr + i)->nama == cari){
-            cout << "\nData ditemukan!\n";
-            cout << "ID    : " << (arr + i)->id << endl;
-            cout << "Nama  : " << (arr + i)->nama << endl;
-
-            swapData((arr + i), (arr + 0));
-            return;
-        }
-    }
-    cout << "\nData tidak ditemukan!\n";
-}
-
-void fibonacciSearch(Hewan *arr){
-    int x;
-    cout << "\nCari ID: ";
-    cin >> x;
-
-    int fib2 = 0, fib1 = 1, fib = 1;
-
-    while(fib < n){
-        fib2 = fib1;
-        fib1 = fib;
-        fib = fib1 + fib2;
-    }
-
-    int offset = -1;
-
-    cout << "\nProses pencarian:\n";
-
-    while(fib > 1){
-        int i = offset + fib2;
-        if(i >= n) i = n - 1;
-
-        cout << "Cek index " << i 
-             << " (ID: " << (arr + i)->id << ")\n";
-
-        if((arr + i)->id < x){
-            fib = fib1;
-            fib1 = fib2;
-            fib2 = fib - fib1;
-            offset = i;
-        }
-        else if((arr + i)->id > x){
-            fib = fib2;
-            fib1 = fib1 - fib2;
-            fib2 = fib - fib1;
-        }
-        else{
-            cout << "\nData ditemukan!\n";
-            cout << "Nama: " << (arr + i)->nama << endl;
-
-            swapData((arr + i), (arr + 0));
-            return;
-        }
-    }
-
-    cout << "\nData tidak ditemukan!\n";
-}
-
-void bubbleSort(Hewan *arr){
-    for(int i = 0; i < n-1; i++){
-        for(int j = 0; j < n-i-1; j++){
-            if((arr + j)->nama > (arr + j + 1)->nama){
-                swapData((arr + j), (arr + j + 1));
-            }
-        }
-    }
-    cout << "\nData diurutkan berdasarkan nama\n";
-}
-
-void selectionSort(Hewan *arr){
-    for(int i = 0; i < n-1; i++){
-        int min = i;
-        for(int j = i+1; j < n; j++){
-            if((arr + j)->harga < (arr + min)->harga){
-                min = j;
-            }
-        }
-        swapData((arr + i), (arr + min));
-    }
-    cout << "\nData diurutkan berdasarkan harga\n";
-}
-
 void enqueue(Hewan *arr){
+    if(rear == MAX-1){
+        cout << "\nAntrian penuh! (Overflow)\n";
+        return;
+    }
+
     int id;
     cout << "\nMasukkan ID hewan: ";
     cin >> id;
 
     for(int i = 0; i < n; i++){
         if((arr + i)->id == id){
-            if(rear == 99){
-                cout << "\nAntrian penuh!\n";
-                return;
-            }
 
             if(front == -1) front = 0;
 
@@ -181,7 +105,7 @@ void enqueue(Hewan *arr){
 
 void dequeue(){
     if(front == -1 || front > rear){
-        cout << "\nAntrian kosong!\n";
+        cout << "\nAntrian kosong! (Underflow)\n";
         return;
     }
 
@@ -189,7 +113,9 @@ void dequeue(){
     cout << "ID   : " << queueArr[front].id << endl;
     cout << "Nama : " << queueArr[front].nama << endl;
 
-    if(top < 99){
+    if(top == MAX-1){
+        cout << "\nRiwayat penuh! (Overflow)\n";
+    } else {
         top++;
         stackArr[top].id = queueArr[front].id;
         stackArr[top].nama = queueArr[front].nama;
@@ -225,7 +151,7 @@ void peekQueue(){
 
 void popRiwayat(){
     if(top == -1){
-        cout << "\nRiwayat kosong!\n";
+        cout << "\nRiwayat kosong! (Underflow)\n";
         return;
     }
 
@@ -268,47 +194,37 @@ void resetData(Hewan* &arr){
 }
 
 int main(){
-    Hewan data[100];
+    Hewan data[MAX];
     Hewan* ptr = data;
 
     int pilih;
 
     do{
-        cout << "\n=========================";
-        cout << "\n=   PAWCARE PETSHOP     =";
-        cout << "\n=========================";
+        cout << "\n=== PAWCARE PETSHOP ===";
         cout << "\n1. Tambah Data";
         cout << "\n2. Tampil Data";
-        cout << "\n3. Linear Search";
-        cout << "\n4. Fibonacci Search";
-        cout << "\n5. Bubble Sort (Nama)";
-        cout << "\n6. Selection Sort (Harga)";
-        cout << "\n7. Reset Data";
-        cout << "\n8. Enqueue (Masuk Antrian)";
-        cout << "\n9. Dequeue (Periksa + Riwayat)";
-        cout << "\n10. Tampil Antrian";
-        cout << "\n11. Pop Riwayat";
-        cout << "\n12. Tampil Riwayat";
-        cout << "\n13. Peek Antrian";
-        cout << "\n14. Peek Riwayat";
+        cout << "\n3. Enqueue";
+        cout << "\n4. Dequeue";
+        cout << "\n5. Tampil Antrian";
+        cout << "\n6. Pop Riwayat";
+        cout << "\n7. Tampil Riwayat";
+        cout << "\n8. Peek Antrian";
+        cout << "\n9. Peek Riwayat";
+        cout << "\n10. Reset";
         cout << "\n0. Keluar";
         cout << "\nPilih: ";
         cin >> pilih;
 
         if(pilih == 1) tambahData(ptr);
         else if(pilih == 2) tampilData(ptr);
-        else if(pilih == 3) linearSearch(ptr);
-        else if(pilih == 4) fibonacciSearch(ptr);
-        else if(pilih == 5) bubbleSort(ptr);
-        else if(pilih == 6) selectionSort(ptr);
-        else if(pilih == 7) resetData(ptr);
-        else if(pilih == 8) enqueue(ptr);
-        else if(pilih == 9) dequeue();
-        else if(pilih == 10) tampilAntrian();
-        else if(pilih == 11) popRiwayat();
-        else if(pilih == 12) tampilRiwayat();
-        else if(pilih == 13) peekQueue();
-        else if(pilih == 14) peekStack();
+        else if(pilih == 3) enqueue(ptr);
+        else if(pilih == 4) dequeue();
+        else if(pilih == 5) tampilAntrian();
+        else if(pilih == 6) popRiwayat();
+        else if(pilih == 7) tampilRiwayat();
+        else if(pilih == 8) peekQueue();
+        else if(pilih == 9) peekStack();
+        else if(pilih == 10) resetData(ptr);
         else if(pilih == 0) cout << "\nProgram selesai\n";
         else cout << "\nPilihan tidak valid!\n";
 
